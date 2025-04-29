ff6795e23e47c4901e91d396bba6bb7403a83c4b
ishay <rachmutishay@gmail.com>
2025-04-29 13:02:32 +0300

    o

--- dal/DataBase.py
+++ dal/DataBase.py
@@ -1,21 +1,23 @@
 import psycopg
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 
 class DataBase:
-
-    DATABASE_PASSWORD = "newpassword"
-    DATABASE_HOST = 'localhost'
-    DATABASE_DBNAME = 'postgres'
-    DATABASE_USER = 'postgres'
-    DATABASE_PORT = '5432'
 
     def __init__(self):
         self.cursor = None
         self.conn = None
 
     def doConnection(self):
-        self.conn = psycopg.connect(dbname='postgres', password='newpassword',
-                                    user='postgres', host='localhost', port='5432')
+        DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+        DATABASE_HOST = os.getenv('DATABASE_HOST')
+        DATABASE_DBNAME = os.getenv('DATABASE_DBNAME')
+        DATABASE_USER = os.getenv('DATABASE_USER')
+        DATABASE_PORT = os.getenv('DATABASE_PORT')
+        self.conn = psycopg.connect(dbname=DATABASE_DBNAME, password=DATABASE_PASSWORD,
+                                    user=DATABASE_USER, host=DATABASE_HOST, port=DATABASE_PORT)
         self.cursor = self.conn.cursor()
         return self.cursor
 
